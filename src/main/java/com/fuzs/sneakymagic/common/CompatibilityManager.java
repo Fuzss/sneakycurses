e8e18f817180be8d43854b30572e61fcb8172acc
Fuzss <mrfusseel@gmail.com>
2021-01-06 18:25:00 +0100

    Config can now properly be reloaded

--- src/main/java/com/fuzs/sneakymagic/common/CompatibilityManager.java
+++ src/main/java/com/fuzs/sneakymagic/common/CompatibilityManager.java
@@ -4,76 +4,85 @@ import com.fuzs.sneakymagic.SneakyMagic;
 import com.fuzs.sneakymagic.config.ConfigBuildHandler;
 import com.fuzs.sneakymagic.mixin.accessor.IEnchantmentAccessor;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.item.*;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 
 public class CompatibilityManager {
 
     private final Map<Enchantment, EnchantmentType> defaultTypes = Maps.newHashMap();
+    private final Set<Set<Enchantment>> previousEnchantments = Sets.newHashSet();
     private final Map<Enchantment, Predicate<Item>> enchantmentPredicates = Maps.newHashMap();
 
     private int typeCounter;
 
     public void load() {
 
-        this.sync(this.getTypesMap());
+        Map<Set<Enchantment>, Predicate<Item>> configMap = this.createConfigMap();
+        if (this.isConfigChanged(configMap)) {
+
+            this.createPredicatesMap(configMap);
+            this.setEnchantmentTypes(this.enchantmentPredicates);
+            this.enchantmentPredicates.clear();
+        }
     }
 
-    private Map<Set<Enchantment>, Predicate<Item>> getTypesMap() {
+    private Map<Set<Enchantment>, Predicate<Item>> createConfigMap() {
 
-        final Map<Set<Enchantment>, Predicate<Item>> types = Maps.newHashMap();
-        types.put(ConfigBuildHandler.swordEnchantments, item -> item instanceof SwordItem && !ConfigBuildHandler.swordBlacklist.contains(item));
-        types.put(ConfigBuildHandler.axeEnchantments, item -> item instanceof AxeItem && !ConfigBuildHandler.axeBlacklist.contains(item));
-        types.put(ConfigBuildHandler.tridentEnchantments, item -> item instanceof TridentItem && !ConfigBuildHandler.tridentBlacklist.contains(item));
-        types.put(ConfigBuildHandler.bowEnchantments, item -> item instanceof BowItem && !ConfigBuildHandler.bowBlacklist.contains(item));
-        types.put(ConfigBuildHandler.crossbowEnchantments, item -> item instanceof CrossbowItem && !ConfigBuildHandler.crossbowBlacklist.contains(item));
+        final Map<Set<Enchantment>, Predicate<Item>> predicates = Maps.newHashMap();
+        predicates.put(ConfigBuildHandler.swordEnchantments, item -> item instanceof SwordItem && !ConfigBuildHandler.swordBlacklist.contains(item));
+        predicates.put(ConfigBuildHandler.axeEnchantments, item -> item instanceof AxeItem && !ConfigBuildHandler.axeBlacklist.contains(item));
+        predicates.put(ConfigBuildHandler.tridentEnchantments, item -> item instanceof TridentItem && !ConfigBuildHandler.tridentBlacklist.contains(item));
+        predicates.put(ConfigBuildHandler.bowEnchantments, item -> item instanceof BowItem && !ConfigBuildHandler.bowBlacklist.contains(item));
+        predicates.put(ConfigBuildHandler.crossbowEnchantments, item -> item instanceof CrossbowItem && !ConfigBuildHandler.crossbowBlacklist.contains(item));
 
-        return types;
+        return predicates;
     }
 
-    private void sync(Map<Set<Enchantment>, Predicate<Item>> types) {
+    private boolean isConfigChanged(Map<Set<Enchantment>, Predicate<Item>> configMap) {
 
-        types.forEach((key, value) -> key.forEach(enchantment -> {
+        Set<Set<Enchantment>> currentEnchantments = configMap.keySet();
+        if (!currentEnchantments.equals(this.previousEnchantments)) {
 
-            // absolutely have to save this in order to prevent a recursive loop and a StackOverflowError
-            // probably necessary due to the way EnchantmentType#create behaves
-            final EnchantmentType originalType = enchantment.type;
-            final EnchantmentType combinedType = EnchantmentType.create(SneakyMagic.MODID.toUpperCase(Locale.ROOT) + "_TYPE_" + typeCounter++,
-                    item -> originalType != null && originalType.canEnchantItem(item) || value.test(item));
-            ((IEnchantmentAccessor) enchantment).setType(combinedType);
-        }));
-    }
-
-    private Predicate<Item> getPredicate(Enchantment enchantment) {
-
-        Optional<Predicate<Item>> predicate = Optional.ofNullable(this.enchantmentPredicates.get(enchantment));
-        if (!predicate.isPresent()) {
-
-            Predicate<Item> defaultPredicate = this.getDefaultPredicate(enchantment);
-            this.enchantmentPredicates.put(enchantment, defaultPredicate);
-            return defaultPredicate;
+            this.previousEnchantments.clear();
+            this.previousEnchantments.addAll(currentEnchantments);
+            return true;
         }
 
-        return predicate.get();
+        return false;
+    }
+
+    private void createPredicatesMap(Map<Set<Enchantment>, Predicate<Item>> configMap) {
+
+        configMap.forEach((key, value) -> key.forEach(enchantment -> this.putPredicate(enchantment, value)));
+    }
+
+    private void putPredicate(Enchantment enchantment, Predicate<Item> predicate) {
+
+        this.enchantmentPredicates.computeIfAbsent(enchantment, this::getDefaultPredicate);
+        this.enchantmentPredicates.merge(enchantment, predicate, ((predicate1, predicate2) -> (item -> predicate1.test(item) || predicate2.test(item))));
     }
 
     private Predicate<Item> getDefaultPredicate(Enchantment enchantment) {
 
-        Optional<EnchantmentType> predicate = Optional.ofNullable(this.defaultTypes.get(enchantment));
-        if (!predicate.isPresent()) {
+        return this.defaultTypes.computeIfAbsent(enchantment, key -> key.type)::canEnchantItem;
+    }
 
-            this.defaultTypes.put(enchantment, enchantment.type);
-            return enchantment.type::canEnchantItem;
-        }
+    private void setEnchantmentTypes(Map<Enchantment, Predicate<Item>> enchantmentPredicates) {
 
-        return predicate.get()::canEnchantItem;
+        enchantmentPredicates.forEach((key, value) -> {
+
+//            System.out.println(ForgeRegistries.ENCHANTMENTS.getKey(key).toString() + ": " + this.typeCounter);
+            final EnchantmentType combinedType = EnchantmentType.create(SneakyMagic.MODID.toUpperCase(Locale.ROOT) + "_TYPE_" + typeCounter++, value);
+            ((IEnchantmentAccessor) key).setType(combinedType);
+        });
     }
 
 }
